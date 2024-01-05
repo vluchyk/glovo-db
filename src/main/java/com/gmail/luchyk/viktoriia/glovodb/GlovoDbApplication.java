9ff60bd094495f12f7b687547d1223f0703a9925
Viktoriia Luchyk <vluchyk@prosoftalliance.com>
2024-01-05 16:38:27 +0200

    init commit

---
+++ src/main/java/com/gmail/luchyk/viktoriia/glovodb/GlovoDbApplication.java
@@ -0,0 +1,13 @@
+package com.gmail.luchyk.viktoriia.glovodb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GlovoDbApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GlovoDbApplication.class, args);
+    }
+
+}
